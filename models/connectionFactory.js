f0a8cd20cfed2ab442f758ec9f73c7cb7f425b5f
RichardDev <richrjweb@yahoo.com.br>
2019-11-03 19:20:50 -0300

    alteracao senha db

--- models/connectionFactory.js
+++ models/connectionFactory.js
@@ -4,7 +4,7 @@ function createDBConnection(){
 		return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
-			password: '3060',
+			password: '',
 			database: 'discodb'			
 		});
 }
