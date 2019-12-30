c56ab262c7f335333ddf16dd86c4d73bccc21968
RichardDev <richrjweb@yahoo.com.br>
2019-12-30 11:32:10 -0300

    change dbmodel

--- discodb.sql
+++ discodb.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 03-Nov-2019 às 19:46
--- Versão do servidor: 5.7.27-0ubuntu0.18.04.1
--- PHP Version: 7.2.19-0ubuntu0.18.04.2
+-- Generation Time: 30-Dez-2019 às 11:05
+-- Versão do servidor: 5.6.39-83.1
+-- versão do PHP: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,17 +19,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `discodb`
+-- Database: `bd`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `colecao`
+-- Estrutura da tabela `lib__colecao`
 --
 
-CREATE TABLE `colecao` (
-  `id` int(10) NOT NULL,
+CREATE TABLE `lib__colecao` (
+  `id` int(11) NOT NULL,
   `nome_colecao` varchar(255) NOT NULL,
   `data_criacao` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -35,12 +37,23 @@ CREATE TABLE `colecao` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `disco`
+-- Estrutura da tabela `lib__colecao_disco`
 --
 
-CREATE TABLE `disco` (
+CREATE TABLE `lib__colecao_disco` (
   `id` int(11) NOT NULL,
   `id_colecao` int(11) NOT NULL,
+  `id_disco` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `lib__disco`
+--
+
+CREATE TABLE `lib__disco` (
+  `id` int(11) NOT NULL,
   `nome_disco` varchar(255) NOT NULL,
   `nome_artista` varchar(255) NOT NULL,
   `estilo` varchar(255) NOT NULL
@@ -51,32 +64,59 @@ CREATE TABLE `disco` (
 --
 
 --
--- Indexes for table `colecao`
+-- Indexes for table `lib__colecao`
 --
-ALTER TABLE `colecao`
+ALTER TABLE `lib__colecao`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `disco`
+-- Indexes for table `lib__colecao_disco`
 --
-ALTER TABLE `disco`
+ALTER TABLE `lib__colecao_disco`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_colecao` (`id_colecao`);
+  ADD KEY `id_colecao` (`id_colecao`),
+  ADD KEY `id_disco` (`id_disco`);
+
+--
+-- Indexes for table `lib__disco`
+--
+ALTER TABLE `lib__disco`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `colecao`
+-- AUTO_INCREMENT for table `lib__colecao`
 --
-ALTER TABLE `colecao`
+ALTER TABLE `lib__colecao`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `disco`
+-- AUTO_INCREMENT for table `lib__colecao_disco`
 --
-ALTER TABLE `disco`
+ALTER TABLE `lib__colecao_disco`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lib__disco`
+--
+ALTER TABLE `lib__disco`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `lib__colecao_disco`
+--
+ALTER TABLE `lib__colecao_disco`
+  ADD CONSTRAINT `fk_id_colecao` FOREIGN KEY (`id_colecao`) REFERENCES `lib__colecao` (`id`),
+  ADD CONSTRAINT `fk_id_disco` FOREIGN KEY (`id_disco`) REFERENCES `lib__disco` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
